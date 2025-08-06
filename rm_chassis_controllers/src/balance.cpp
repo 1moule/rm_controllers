1c9641a796230e5756fe7b4acb578381b9222375
1moule <1961466188@qq.com>
2025-08-06 21:16:16 +0800

    Add vel_z des.

--- rm_chassis_controllers/src/balance.cpp
+++ rm_chassis_controllers/src/balance.cpp
@@ -214,7 +214,7 @@ void BalanceController::normal(const ros::Time& time, const ros::Duration& perio
   auto x = x_;
   x(3) -= vel_cmd_.x;
   u = k_ * (-x);
-  pid_yaw_vel_.computeCommand(0.0 - angular_vel_base_.z, period);
+  pid_yaw_vel_.computeCommand(vel_cmd_.z - angular_vel_base_.z, period);
   pid_vel_x_.computeCommand(vel_cmd_.x - x_[3], period);
   if (state_pub_->trylock())
   {
