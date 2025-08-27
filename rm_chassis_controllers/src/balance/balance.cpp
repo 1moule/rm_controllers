b80410e385ccb98ffdd2141606ce95c3c3ca3d07
1moule <1961466188@qq.com>
2025-08-27 12:47:03 +0800

    Knock on the stairs.

--- rm_chassis_controllers/src/balance/balance.cpp
+++ rm_chassis_controllers/src/balance/balance.cpp
@@ -573,13 +573,27 @@ void BalanceController::sitDown(const ros::Time& time, const ros::Duration& peri
     ROS_INFO("[balance] Enter SIT_DOWN");
     balance_state_changed_ = true;
   }
+  Eigen::Matrix<double, 2, 1> F_leg;
+  double T_theta_l, T_theta_r;
+  F_leg[0] = pid_left_leg_.computeCommand(0.1 - left_pos_[0], period);
+  F_leg[1] = pid_right_leg_.computeCommand(0.1 - right_pos_[0], period);
+  T_theta_l = pid_left_leg_theta_.computeCommand(-0.1 - left_pos_[1], period);
+  T_theta_r = pid_right_leg_theta_.computeCommand(-0.1 - right_pos_[1], period);
+  double left_T[2], right_T[2];
+  leg_conv(F_leg[0], -T_theta_l, left_angle[0], left_angle[1], left_T);
+  leg_conv(F_leg[1], -T_theta_r, right_angle[0], right_angle[1], right_T);
+  left_first_leg_joint_handle_.setCommand(left_T[0]);
+  right_first_leg_joint_handle_.setCommand(right_T[0]);
+  left_second_leg_joint_handle_.setCommand(left_T[1]);
+  right_second_leg_joint_handle_.setCommand(right_T[1]);
   left_wheel_joint_handle_.setCommand(pid_left_wheel_vel_.computeCommand(-joint_handles_[0].getVelocity(), period));
   right_wheel_joint_handle_.setCommand(pid_right_wheel_vel_.computeCommand(-joint_handles_[1].getVelocity(), period));
-  left_first_leg_joint_handle_.setCommand(0.);
-  left_second_leg_joint_handle_.setCommand(0.);
-  right_first_leg_joint_handle_.setCommand(0.);
-  right_second_leg_joint_handle_.setCommand(0.);
-  if (abs(x_left_(1)) < 0.1 && abs(x_left_(5)) < 0.1 && abs(x_left_(3)) < 0.05)
+  //  left_first_leg_joint_handle_.setCommand(0.);
+  //  left_second_leg_joint_handle_.setCommand(0.);
+  //  right_first_leg_joint_handle_.setCommand(0.);
+  //  right_second_leg_joint_handle_.setCommand(0.);
+  //  if (abs(x_left_(1)) < 0.1 && abs(x_left_(5)) < 0.1 && abs(x_left_(3)) < 0.05)
+  if (left_pos_[0] < 0.15 && right_pos_[0] < 0.15)
   {
     balance_mode_ = BalanceMode::STAND_UP;
     balance_state_changed_ = false;
